6a01020dad44ac4fb4e012ed3eab677db86489ff
Díaz Urbaneja Víctor Eduardo Diex <diaz.victor@openmailbox.org>
2019-05-08 12:57:00 -0400

    Añadida la libreria, la plantilla de gtk y el archivo principal

---
+++ init.lua
@@ -0,0 +1,27 @@
+#!/usr/bin/lua5.1
+--[[--
+ @package   Chateando
+ @filename  init.lua
+ @version   1.0
+ @autor     Díaz Urbaneja Víctor Eduardo Diex <diaz.victor@openmailbox.org>
+ @date      08.04.2019 15:40:38 -04
+]]
+
+require('lib.middleclass') -- la libreria middleclass me da soporte a OOP
+local mqtt = require('mosquitto') -- la libreria que soporta el protocolo
+local lgi = require('lgi') -- requiero esta libreria que me permitira usar GTK
+
+local GObject = lgi.GObject -- parte de lgi
+local GLib = lgi.GLib -- para el treeview
+
+local Gtk = lgi.require('Gtk', '3.0') -- el objeto GTK
+
+local assert = lgi.assert
+local builder = Gtk.Builder()
+
+assert(builder:add_from_file('MainWindow.ui'),"error al cargar el archivo") -- hago un debugger, si el archivo existe (true) enlaso el archivo "MainWindow.ui", si no existe (false) imprimo un error
+
+local ui = builder.objects
+local main_window = ui.main_window -- invoco la ventana con el id main_window
+local about_window = ui.about_window -- invoco la ventana con el id about_window
+local login_window = ui.login_window -- invoco la ventana con el id login_window
