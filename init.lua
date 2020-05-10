2a7f36a6e7a7c7dea1c2684fa70e5dd155f66066
Victor Diego Alegandro Diaz Urbaneja <sodomon2@gmail.com>
2020-05-09 22:57:18 -0400

    Añadiendo trayicon y arreglando un poco la identacion

--- init.lua
+++ init.lua
@@ -7,41 +7,42 @@
  @date      15:40:38 -04 2019
 ]]
 
-require('lib.middleclass')              										-- la libreria middleclass me da soporte a OOP
-local mqtt = require('mosquitto')     											-- la libreria que soporta el protocolo
-local json = require('lib.json')     											-- la libreria que soporta json
-local lgi = require('lgi')            											-- requiero esta libreria que me permitira usar GTK
+require('lib.middleclass')              						                -- la libreria middleclass me da soporte a OOP
+local mqtt    = require('mosquitto')     						                -- la libreria que soporta el protocolo
+local json    = require('lib.json')     						                -- la libreria que soporta json
+local lgi     = require('lgi')            						                -- requiero esta libreria que me permitira usar GTK
 
-local GObject = lgi.GObject             										-- parte de lgi
-local GLib = lgi.GLib                  											-- para el treeview
-local Gdk = lgi.Gdk                  											-- para el treeview
-local Gtk = lgi.require('Gtk', '3.0')											-- el objeto GTK
+local GObject = lgi.GObject             						                -- parte de lgi
+local GLib    = lgi.GLib                  						                -- para el treeview
+local Gdk     = lgi.Gdk                  						                -- para el treeview
+local Gtk     = lgi.require('Gtk', '3.0')						                -- el objeto GTK
 
-local assert = lgi.assert
+local assert  = lgi.assert
 local builder = Gtk.Builder()
 
 assert(builder:add_from_file('MainWindow.ui'), "error al cargar el archivo") 	-- hago un debugger, si este archivo existe (true) enlaso el archivo ejemplo.ui, si no existe (false) imprimo un error
 
-local main_window = builder:get_object('main_window')          					-- invoco la ventana con el id main_window
-local about_window = builder:get_object('about_window')        					-- invoco la ventana con el id about_window
-local login_window = builder:get_object('login_window')        					-- invoco la ventana con el id login_window
+local main_window     = builder:get_object('main_window')          				-- invoco la ventana con el id main_window
+local about_window    = builder:get_object('about_window')        			    -- invoco la ventana con el id about_window
+local login_window    = builder:get_object('login_window')        				-- invoco la ventana con el id login_window
 
-local header_bar = builder:get_object('header_bar')
-local list_users = builder:get_object('list_users')
+local header_bar      = builder:get_object('header_bar')
+local list_users      = builder:get_object('list_users')
 
-local  messages = builder:get_object('mensajes')            					-- invoco al
-local  entry_message = builder:get_object('entry_mensaje')      				-- invoco al boton con el id load_choser
-local  entry_user = builder:get_object('entry_user')       						-- invoco al boton con el id load_choser
+local  messages       = builder:get_object('mensajes')            				-- invoco al
+local  entry_message  = builder:get_object('entry_mensaje')      				-- invoco al boton con el id load_choser
+local  entry_user     = builder:get_object('entry_user')       					-- invoco al boton con el id load_choser
 local  entry_password = builder:get_object('entry_password')    				-- invoco al boton con el id load_choser
-local  entry_broker = builder:get_object('entry_broker')       					-- invoco al boton con el id load_choser
-local  entry_port = builder:get_object('entry_port')       						-- invoco al boton con el id load_choser
-local  entry_topic = builder:get_object('entry_topic')     						-- invoco al boton con el id load_choser
-local  buffer = builder:get_object('buffer_mensajes')     						-- invoco al boton con el id btn_correr
+local  entry_broker   = builder:get_object('entry_broker')       				-- invoco al boton con el id load_choser
+local  entry_port     = builder:get_object('entry_port')       					-- invoco al boton con el id load_choser
+local  entry_topic    = builder:get_object('entry_topic')     					-- invoco al boton con el id load_choser
+local  buffer         = builder:get_object('buffer_mensajes')     				-- invoco al boton con el id btn_correr
 
-local  btn_submit = builder:get_object('btn_enviar')          					-- invoco al boton con el id btn_cerrar
-local  btn_about = builder:get_object('btn_about')          					-- invoco al boton con el id btn_cerrar
-local  btn_login = builder:get_object('btn_login')          					-- invoco al boton con el id btn_cerrar
-local  btn_cancel = builder:get_object('btn_cancel')        					-- invoco al boton con el id btn_cerrar
+local  btn_submit     = builder:get_object('btn_enviar')          				-- invoco al boton con el id btn_cerrar
+local  btn_about      = builder:get_object('btn_about')          				-- invoco al boton con el id btn_cerrar
+local  btn_login      = builder:get_object('btn_login')          				-- invoco al boton con el id btn_cerrar
+local  btn_cancel     = builder:get_object('btn_cancel')        				-- invoco al boton con el id btn_cerrar
+local  mqtt_tray      = builder:get_object('mqtt_tray')        					-- invoco al boton con el id btn_cerrar
 
 local broker_default = 'broker.mqtt.cool'  										-- el servidor por defecto
 local port_default = '1883'   													-- el puerto por defecto
@@ -124,7 +125,7 @@ function validate_logIn()
 	        msg = json:decode(payload)
 	    end
 	end
-
+    
 	client:loop_start()
 	channel = entry_topic.text
 
@@ -201,6 +202,21 @@ GLib.timeout_add (
 
 function btn_submit:on_clicked()
     submit()
+end
+
+local visible = false
+
+function tray()
+	visible = not visible
+	if visible then
+		main_window:show_all()
+	else
+		main_window:hide()
+	end
+end
+
+function mqtt_tray:on_activate()
+	tray()
 end
 
 function login_window:on_destroy()
