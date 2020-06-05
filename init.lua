76aca2074dbbdf5198f2de924e09445be1530803
diazvictor <diaz.victor@openmailbox.org>
2020-06-05 18:59:43 -0400

    codigo modificado para el llamado de objetos mediante el prefijo ui

--- init.lua
+++ init.lua
@@ -7,7 +7,7 @@
  @date      14.05.2020 21:25:45 -04
 ]]
 
-CSD_ENABLED = true
+CSD_ENABLED = false
 if CSD_ENABLED then win = 'view/MainCSD.ui' else win = 'view/MainNoCSD.ui' end
 
       class   = require('lib.middleclass')              						-- la libreria middleclass me da soporte a OOP
@@ -25,35 +25,9 @@ local assert  = lgi.assert
 local builder = Gtk.Builder()
 
 assert(builder:add_from_file(win), "The window file failed loading!") 	        -- hago un debugger, si este archivo existe (true) enlaso el archivo ejemplo.ui, si no existe (false) imprimo un error
+local ui = builder.objects
 
 setLang("en_us")
-
-local main_window     = builder:get_object('main_window')          				-- invoco la ventana con el id main_window
-local about_window    = builder:get_object('about_window')        			    -- invoco la ventana con el id about_window
-local login_window    = builder:get_object('login_window')        				-- invoco la ventana con el id login_window
-
-local header_bar      = builder:get_object('header_bar')
-local list_users      = builder:get_object('list_users')
-
-local  messages       = builder:get_object('mensajes')            				-- invoco al
-local  entry_message  = builder:get_object('entry_mensaje')      				-- invoco al boton con el id load_choser
-local  entry_user     = builder:get_object('entry_user')       					-- invoco al boton con el id load_choser
-local  entry_password = builder:get_object('entry_password')    				-- invoco al boton con el id load_choser
-local  entry_broker   = builder:get_object('entry_broker')       				-- invoco al boton con el id load_choser
-local  entry_port     = builder:get_object('entry_port')       					-- invoco al boton con el id load_choser
-local  entry_topic    = builder:get_object('entry_topic')     					-- invoco al boton con el id load_choser
-local  buffer         = builder:get_object('buffer_mensajes')     				-- invoco al boton con el id btn_correr
-
-local  btn_submit     = builder:get_object('btn_submit')          				-- invoco al boton con el id btn_cerrar
-if CSD_ENABLED then
-	btn_about		  = builder:get_object('btn_about')          				-- invoco al boton con el id btn_cerrar
-else
-	menu_about  	  = builder:get_object('about_submenu')          			-- invoco al boton con el id btn_cerrar
-	menu_quit  		  = builder:get_object('quit_submenu')          			-- invoco al boton con el id btn_cerrar
-end
-local  btn_login      = builder:get_object('btn_login')          				-- invoco al boton con el id btn_cerrar
-local  btn_cancel     = builder:get_object('btn_cancel')        				-- invoco al boton con el id btn_cerrar
-local  mqtt_tray      = builder:get_object('mqtt_tray')        					-- invoco al boton con el id btn_cerrar
 
 local possible_characters = 'abcdefghijklmnopqrstuvwyxz1234567890'              -- caracteres posibles para el generador de passwords
 local broker_default = 'broker.mqtt.cool'  										-- el servidor por defecto
@@ -62,8 +36,8 @@ local keepalive = 60     														-- si me desconecto, ¿Cuanto tiempo espe
 local qos = 2        															-- acuse de recibo
 local username_default = 'MoonZaphire' .. math.random(1, 999) 					-- usuario por defecto
 local topic_default = "users/chat"
-entry_user.text = username_default												-- mostrar usuario aleatorio
-entry_topic.text = topic_default
+ui.entry_user.text = username_default												-- mostrar usuario aleatorio
+ui.entry_topic.text = topic_default
 local password_default = ''														-- contraseña por defecto
 msg = nil
 
@@ -72,34 +46,33 @@ local usuarios = {}
 user, pass = nil, nil
 
 function user_list()
-	list_users:clear()
+	ui.list_users:clear()
 	for i, item in pairs(usuarios) do
-		list_users:append({
+		ui.list_users:append({
 			item
 		})
 	end
 end
 
+--/////////////////////////////////////////////////////////////////////////////////////////////--
 
-	--/////////////////////////////////////////////////////////////////////////////////////////////--
+ui.btn_submit.label = getLINE("send")
+ui.btn_login.label = getLINE("login")
+ui.btn_cancel.label = getLINE("cancel")
 
-		builder:get_object('btn_submit').label = getLINE("send")
-		builder:get_object('btn_login').label = getLINE("login")
-		builder:get_object('btn_cancel').label = getLINE("cancel")
+ui.users.title = getLINE("users")
+ui.label_user.label = getLINE("user")
+ui.label_pass.label = getLINE("password")
+ui.label_broker.label = getLINE("broker")
+ui.label_port.label = getLINE("port")
+ui.label_topic.label = getLINE("topic")
 
-		builder:get_object('usuarios').title = getLINE("users")
-		builder:get_object('label_user').label = getLINE("user")
-		builder:get_object('label_pass').label = getLINE("password")
-		builder:get_object('label_broker').label = getLINE("broker")
-		builder:get_object('label_port').label = getLINE("port")
-		builder:get_object('label_topic').label = getLINE("topic")
+ui.entry_user.placeholder_text = username_default
+ui.entry_password.placeholder_text = getLINE("funny_pasw") --this doesnt works for some reason
 
-		builder:get_object('entry_user').placeholder_text = username_default
-		builder:get_object('entry_password').placeholder_text = getLINE("funny_pasw") --this doesnt works for some reason
+ui.login_window.title = getLINE("login")
 
-		builder:get_object('login_window').title = getLINE("login")
-
-	--/////////////////////////////////////////////////////////////////////////////////////////////--
+--/////////////////////////////////////////////////////////////////////////////////////////////--
 
 local function superpass()
 	math.randomseed(os.time())
@@ -117,9 +90,9 @@ local function superpass()
 end
 
 function validate_logIn()
-	username, password = entry_user.text:gsub("%s", ""):gsub(" ", ""), entry_password.text
+	username, password = ui.entry_user.text:gsub("%s", ""):gsub(" ", ""), ui.entry_password.text
 
-	broker, port = entry_broker.text, entry_port.text
+	broker, port = ui.entry_broker.text, ui.entry_port.text
 
 	local _e = false
 	if (#username < 1) then
@@ -177,13 +150,13 @@ function validate_logIn()
 	end
 
 	client:loop_start()
-	channel = entry_topic.text
+	channel = ui.entry_topic.text
 
     client:subscribe( channel, 0 )
 	if CSD_ENABLED then
-		header_bar.title = '#' .. channel
+		ui.header_bar.title = '#' .. channel
 	else
-		main_window.title = '#' .. channel
+		ui.main_window.title = '#' .. channel
 	end
 
     client:subscribe( 'users/connect', 0 )
@@ -198,19 +171,19 @@ function validate_logIn()
 	client:publish( channel, json:encode( message_connect ) )
 end
 
-function btn_login:on_clicked()
+function ui.btn_login:on_clicked()
 	validate_logIn()
 
-	login_window:hide()
-	main_window:show_all()
-	mqtt_tray.visible = true
+	ui.login_window:hide()
+	ui.main_window:show_all()
+	ui.mqtt_tray.visible = true
 end
 
-entry_message:grab_focus()												-- al iniciar lo mantengo en estado focus (activo)
+ui.entry_message:grab_focus()												-- al iniciar lo mantengo en estado focus (activo)
 -- cuando se presione Enviar
 
 function submit()
-    local msje = tostring(entry_message.text)
+    local msje = tostring(ui.entry_message.text)
     if ( msje ~= '' ) then                  							-- solo envio si hay mensaje
 		local info = {
 			user = user,
@@ -218,35 +191,35 @@ function submit()
 			time = os.date('%H:%M:%S')
 		}
         client:publish( channel, tostring( json:encode( info ) ) )		-- mando el mensaje
-        entry_message.text = '' 										--limpio el entry
+        ui.entry_message.text = '' 										--limpio el entry
     end
-    entry_message:grab_focus()											-- lo mantengo siempre en estado focus (activo)
+    ui.entry_message:grab_focus()											-- lo mantengo siempre en estado focus (activo)
 end
 
-local mark = buffer:create_mark( nil, buffer:get_end_iter(), false )
+local mark = ui.buffer_messages:create_mark( nil, ui.buffer_messages:get_end_iter(), false )
 GLib.timeout_add (
     GLib.PRIORITY_DEFAULT, 300,
 	function ()
 		if msg then
 			if (msg.user == '') then
-				buffer:insert(buffer:get_iter_at_mark(mark),
+				ui.buffer_messages:insert(ui.buffer_messages:get_iter_at_mark(mark),
 					'\n' ..
 					('%s %s'):format(msg.time, msg.msg), -- esto es el mensaje recibido
 				-1)
 			else
-				buffer:insert(buffer:get_iter_at_mark(mark),
+				ui.buffer_messages:insert(ui.buffer_messages:get_iter_at_mark(mark),
 					'\n' ..
 					('%s [%s]: %s'):format(msg.time, msg.user, msg.msg), -- esto es el mensaje recibido
 				-1)
 			end
-			messages:scroll_mark_onscreen(mark)
+			ui.messages:scroll_mark_onscreen(mark)
 			msg = nil
 		end
 		return true
 	end
 )
 
-function entry_message:on_key_release_event(env)
+function ui.entry_message:on_key_release_event(env)
 	-- al presionar la tecla Enter/Return
     if ( env.keyval  == Gdk.KEY_Return ) then
 		-- llamo a la funcion de submit que envia un mensaje
@@ -263,22 +236,21 @@ GLib.timeout_add (
 	end
 )
 
-function btn_submit:on_clicked()
+function ui.btn_submit:on_clicked()
     submit()
 end
 
 if CSD_ENABLED then
-	function btn_about:on_clicked()
-		about_window:run()
-		about_window:hide()
+	function ui.btn_about:on_clicked()
+		ui.about_window:run()
+		ui.about_window:hide()
 	end
 else
-	function menu_about:on_button_press_event()
-		about_window:run()
-		about_window:hide()
+	function ui.about_submenu:on_button_press_event()
+		ui.about_window:run()
+		ui.about_window:hide()
 	end
-	function menu_quit:on_button_press_event()
-		quit_app()
+	function ui.quit_submenu:on_button_press_event()
 	    Gtk.main_quit()
 	end
 end
@@ -287,30 +259,30 @@ local visible = false
 
 function tray()
 	visible = not visible
-    if ( login_window.is_active == true ) or ( login_window.is_active == false ) then
+    if ( ui.login_window.is_active == true ) or ( ui.login_window.is_active == false ) then
 		if ( visible ) then
-			main_window:show_all()
+			ui.main_window:show_all()
         else
-            main_window:hide()
+            ui.main_window:hide()
         end
     end
 end
 
-function mqtt_tray:on_activate()
+function ui.mqtt_tray:on_activate()
 	tray()
 end
 
-function login_window:on_destroy()
+function ui.login_window:on_destroy()
 	Gtk.main_quit()
 end
 
-function btn_cancel:on_clicked()
+function ui.btn_cancel:on_clicked()
 	Gtk.main_quit()
 end
 
-function main_window:on_destroy()
+function ui.main_window:on_destroy()
     Gtk.main_quit()
 end
 
-login_window:show_all()
+ui.login_window:show_all()
 Gtk.main()
