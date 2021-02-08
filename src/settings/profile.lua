4c94d1cfd2fee7fc1b561d9c1cd174d069ac8ab1
diazvictor <diaz.victor@openmailbox.org>
2021-02-07 23:35:20 -0400

    New module: here I edit the profile

---
+++ src/settings/profile.lua
@@ -0,0 +1,32 @@
+--[[--
+ @package   MoonZaphire
+ @filename  settings/profile.lua
+ @version   1.0
+ @author    Díaz Urbaneja Víctor Eduardo Diex <victor.vector008@gmail.com>
+ @date      07.02.2021 21:24:24 -04
+]]
+
+--- I create the SettingsAbout subclass of MoonZaphire
+MoonZaphire:class('SettingsProfile', Gtk.Box)
+
+--- At the beginning of the class
+function MoonZaphire.SettingsProfile:_class_init(klass)
+	--- I load the template
+	klass:set_template_from_resource(
+		'/com/github/diazvictor/MoonZaphire/data/ui/settings/profile.ui'
+	)
+	--- I add the desired elements to the template
+	-- klass:bind_template_child_full('btn_close', true, 0)
+	--- Miqueas says that here (_class_init) register a method/function
+end
+
+--- When building the class
+function MoonZaphire.SettingsProfile:_init()
+	-- Start template
+	self:init_template()
+
+	-- I load the template objects
+	-- local search = self:get_template_child(
+		-- MoonZaphire.SettingsProfile, 'btn_close'
+	-- )
+end
