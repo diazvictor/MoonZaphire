70fa3cf637be3937721e646837f7b8742182d2cb
diazvictor <diaz.victor@openmailbox.org>
2021-02-07 23:33:47 -0400

    New changes in the template: * The ids have been changed * Added a button panel New code changes: * The ids have been changed * Search button now works

--- src/window.lua
+++ src/window.lua
@@ -17,11 +17,11 @@ function MoonZaphire.Window:_class_init(klass)
 	)
 
 	--- I add the desired elements to the template
-	klass:bind_template_child_full('Background', true, 0)
-	klass:bind_template_child_full('Content', true, 0)
-	klass:bind_template_child_full('Login', true, 0)
-	klass:bind_template_child_full('search_members', true, 0)
+	klass:bind_template_child_full('background', true, 0)
+	klass:bind_template_child_full('content', true, 0)
+	klass:bind_template_child_full('login', true, 0)
 	klass:bind_template_child_full('btn_menu', true, 0)
+	klass:bind_template_child_full('btn_search', true, 0)
 end
 
 --- When building the class
@@ -32,33 +32,35 @@ function MoonZaphire.Window:_init()
 	-- I load the template objects
 	-- @TODO: Find a way to inherit objects to other classes without declaring
 	-- them as global classes
-	background = self:get_template_child(MoonZaphire.Window, 'Background')
-	content = self:get_template_child(MoonZaphire.Window, 'Content')
-	content_login = self:get_template_child(MoonZaphire.Window, 'Login')
+	background = self:get_template_child(MoonZaphire.Window, 'background')
+	content = self:get_template_child(MoonZaphire.Window, 'content')
+	content_login = self:get_template_child(MoonZaphire.Window, 'login')
 	local btn_menu = self:get_template_child(MoonZaphire.Window, 'btn_menu')
-	local search_members = self:get_template_child(MoonZaphire.Window, 'search_members')
+	local btn_search = self:get_template_child(MoonZaphire.Window, 'btn_search')
 
 	btn_menu.on_clicked = function (self)
 		background:add_overlay(
 			MoonZaphire.Settings {
-				id = 'Modal'
+				id = 'Settings'
 			}
 		)
-		utils:addClass(background.child.Modal, 'alert-dialog')
+		utils:addClass(background.child.Settings, 'alert-dialog')
 	end
 
-	search_members.on_key_release_event = function (self, env)
-		if ( env.keyval  == Gdk.KEY_Return ) then
-			if (self.text ~= '') then
-				local id_member = tostring(os.time())
-				local member = MoonZaphire.Roster:add_member(id_member)
-				-- member.child.fullname.label = self.text
-			end
+	--- By clicking I search a roster
+	btn_search.on_toggled = function (self)
+		if  (self.active) then
+			MoonZaphire.Roster:show_search(true)
+		else
+			MoonZaphire.Roster:show_search(false)
 		end
 	end
+
 	-- I load my css styles
 	local styles = Gtk.CssProvider()
-	styles:load_from_resource("/com/github/diazvictor/MoonZaphire/data/styles/custom.css")
+	styles:load_from_resource(
+		'/com/github/diazvictor/MoonZaphire/data/styles/custom.css'
+	)
 
 	self:get_style_context().add_provider_for_screen(
 		self:get_screen(self),
